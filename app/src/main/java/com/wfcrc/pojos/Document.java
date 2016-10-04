6bd4cd4d8a04938e2eab6b8d94f0a696d2fccbb3
malonsolopez <malonsolopez@gmail.com>
2016-10-03 18:51:31 -0700

    Document POJO and JSON

---
+++ app/src/main/java/com/wfcrc/pojos/Document.java
@@ -0,0 +1,103 @@
+package com.wfcrc.pojos;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/**
+ * Created by maria on 8/29/16.
+ */
+public class Document implements Serializable{
+
+    private String title;
+    private String format;
+    private String category;
+    private String url;
+
+    public Document(){
+
+    }
+
+    public Document(String title, String format, String category, String url) {
+        this.title = title;
+        this.format = format;
+        this.category = category;
+        this.url = url;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    // Decodes program json into program model object
+    public static Document fromJson(JSONObject jsonObject) {
+        Document program = new Document();
+        // Deserialize json into object fields
+        try {
+            program.title = jsonObject.getString("title");
+            program.format = jsonObject.getString("format");
+            program.category = jsonObject.getString("category");
+            program.url = jsonObject.getString("url");
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+        // Return new object
+        return program;
+    }
+
+    // Decodes array of program json results into program model objects
+    public static ArrayList<Document> fromJson(String strJson) throws JSONException {
+        JSONObject  jsonRootObject = new JSONObject(strJson);
+        //Get the instance of JSONArray that contains JSONObjects
+        JSONArray jsonArray = jsonRootObject.optJSONArray("program");
+        JSONObject programJson;
+        ArrayList<Document> programs = new ArrayList<Document>(jsonArray.length());
+        // Process each result in json array, decode and convert to business object
+        for (int i=0; i < jsonArray.length(); i++) {
+            try {
+                programJson = jsonArray.getJSONObject(i);
+            } catch (Exception e) {
+                e.printStackTrace();
+                continue;
+            }
+
+            Document program = Document.fromJson(programJson);
+            if (program != null) {
+                programs.add(program);
+            }
+        }
+        return programs;
+    }
+}
