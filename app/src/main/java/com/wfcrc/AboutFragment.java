1137a06403cffad1d132961034d608264527c2c3
malonsolopez <malonsolopez@gmail.com>
2016-10-13 17:12:16 -0700

    Share option on About Us screen

--- app/src/main/java/com/wfcrc/AboutFragment.java
+++ app/src/main/java/com/wfcrc/AboutFragment.java
@@ -7,11 +7,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.wfcrc.social.FollowUsImp;
+import com.wfcrc.social.Share;
 
 
 /**
@@ -98,6 +100,24 @@ public class AboutFragment extends Fragment {
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         inflater.inflate(R.menu.about_menu, menu);
         super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.share) {
+            //TODO: content for sharing
+            String subject ="";
+            String body ="";
+            (new Share(getActivity(), subject, body)).share();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
