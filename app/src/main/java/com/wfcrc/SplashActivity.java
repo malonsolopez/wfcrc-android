41b5082187152af2b43819768d43a22dec3cfa8a
malonsolopez <malonsolopez@gmail.com>
2017-09-18 18:12:10 -0700

    Add progress bar to the splash screen

--- app/src/main/java/com/wfcrc/SplashActivity.java
+++ app/src/main/java/com/wfcrc/SplashActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ProgressBar;
 
 import com.wfcrc.repository.FTPDocumentRepository;
 import com.wfcrc.repository.RepositoryException;
@@ -16,6 +17,8 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //show progress bar
+        setContentView(R.layout.activity_splash);
         //retrieve document gallery
         try {
             //(new WordPressDocumentRepository(this, new SplashVolleyCallback(this))).getAll();
